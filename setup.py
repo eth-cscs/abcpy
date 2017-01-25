01da20f7b3be14b7ed7a10ed4eb79a5ab851c583
Marcel Schoengens <mschoengens@bitvalve.org>
2017-01-25 18:01:32 +0100

    Adaptions required for rtfd.io.

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     # install_requires=['numpy', 'scipy'],
-    install_requires=dependencies,
+    # install_requires=dependencies,
 
 
     # List additional groups of dependencies here (e.g. development
