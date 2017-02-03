c381da944f938a655ae0b350397ebfa668e4f3e5
Marcel Schoengens <mschoengens@bitvalve.org>
2017-02-03 10:39:58 +0100

    Bugfix.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 with open(path.join(here, 'requirements.txt')) as f:
