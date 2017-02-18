3d709a52d655da5697f0d1737ea4f084cd3b963c
statrita <statrita2004@gmail.com>
2017-02-18 21:34:14 +0100

    One model selection rule using Random Forest (Pudlo, 2016) implemented

--- abcpy/modelselections.py
+++ abcpy/modelselections.py
@@ -1,26 +1,24 @@
 from abc import ABCMeta, abstractmethod
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
 
 
 class ModelSelections(metaclass = ABCMeta):
-    """This abstract base class defines how to calculate statistics from dataset.
-
-    The base class also implements a polynomial expansion with cross-product
-    terms that can be used to get desired polynomial expansion of the calculated statistics.
-    
+    """This abstract base class defines a model selection rulr of how to choose model from a set of models
+    given an observation.
+ 
             
     """
     
     @abstractmethod
-    def __init__(self, model_array, statistics_calc, distance_calc, backend, seed = None):
+    def __init__(self, model_array, statistics_calc, backend, seed = None):
         """Constructor that must be overwritten by the sub-class.
 
-        The constructor of a sub-class must accept arguments for the polynomial 
-        expansion after extraction of the summary statistics, one has to define 
-        the degree of polynomial expansion and cross, indicating whether cross-prodcut
-        terms are included.
+        The constructor of a sub-class must accept an array of models to choose the model 
+        from, and two non-optional parameters statistics calculator and backend stored in self.statistics_calc 
+        and self.backend defining how to calculate sumarry statistics from data and what kind of parallelization
+        to use.
         
         Parameters
         ----------
@@ -28,8 +26,6 @@ class ModelSelections(metaclass = ABCMeta):
             A list of models which are of type abcpy.models.Model
         statistics: abcpy.statistics.Statistics
             Statistics object that conforms to the Statistics class.
-        distance: abcpy.distances.Distance
-            Distance object that conforms to the Distance class.
         backend: abcpy.backends.Backend
             Backend object that conforms to the Backend class.
         seed: integer, optional
@@ -37,7 +33,6 @@ class ModelSelections(metaclass = ABCMeta):
         """   
         self.model_array = model_array
         self.statistics_calc = statistics_calc
-        self.distance_calc = distance_calc
         self.backend = backend
         self.rng = np.random.RandomState(seed)
         self.reference_table_calculated = None
@@ -74,8 +69,8 @@ class ModelSelections(metaclass = ABCMeta):
 
     @abstractmethod
     def posteriorprobability(self, observations):
-        """To be overwritten by any sub-class: returns the approximate posterior probabilities 
-        of the models in model_array given the observed data set observations. It is assumed that observations 
+        """To be overwritten by any sub-class: returns the approximate posterior probability
+        of the chosen model given the observed data set observations. It is assumed that observations 
         is a  list of n same type elements(eg., The observations can be a list containing n timeseries, n graphs or n np.ndarray).
         
         Parameters
@@ -91,118 +86,6 @@ class ModelSelections(metaclass = ABCMeta):
         """
         
         raise NotImplemented
-
-class Knn(ModelSelections):
-    """
-    This class implements the model selection procedure based on the k-nearest neighbour 
-    classifier as described in Stoehr et. al. [1].
-    
-    [1] Stoehr, J., Pudlo, P. and Cucala, L. (2015). Adaptive ABC model choice and geometric
-    summary statistics for hidden Gibbs random fields. Statistics and Computing, 25, 129-141.
-    """
-    def __init__(self, model_array, statistics_calc, distance_calc, backend, seed = None):
-        self.model_array = model_array
-        self.statistics_calc = statistics_calc
-        self.distance_calc = distance_calc
-        self.backend = backend
-        self.rng = np.random.RandomState(seed)  
-        self.reference_table_calculated = 0
-
-    def modelchoice(self, observations, n_samples = 1000, n_samples_per_param = 100, k = 50):
-        """        
-        Parameters
-        ----------
-        data: python list
-            Contains n data sets.
-        n_samples : integer, optional
-            Number of samples to generate for reference table.
-        n_samples_per_param : integer, optional 
-            Number of data points in each simulated data set. 
-        k : integer, optional
-            The parameter for K-nearest neighbour classifier. The default calue if 50
-        Returns
-        -------
-        abcpy.models.Model
-            A model which are of type abcpy.models.Model
-            
-        """
-        
-        # Creation of reference table
-        if self.reference_table_calculated is 0:        
-            rc = _RemoteContextgeneratereferencetable(self.backend, self.model_array, self.distance_calc, self.statistics_calc, observations, n_samples_per_param)
-
-            # Simulating the data, distance and statistics                
-            seed_arr = self.rng.randint(1, n_samples*n_samples, size=n_samples, dtype=np.int32)
-            seed_pds = self.backend.parallelize(seed_arr)     
-
-            model_data_pds = self.backend.map(rc._simulate_model_data, seed_pds)
-            model_data = self.backend.collect(model_data_pds)
-            models, data, distances, statistics = [list(t) for t in zip(*model_data)]
-            self.reference_table_models = models
-            self.reference_table_data = data
-            self.reference_table_distances = np.array(distances)
-            self.reference_table_statistics = statistics
-            self.reference_table_calculated = 1
-        
-        # Sort the array
-        Indices = np.array(sorted(range(len(self.reference_table_distances)), key=lambda k: self.reference_table_distances[k])).astype(int)
-
-        # Compute the frequence of each model chosen in the k models having the smalles distance
-        count = np.zeros(shape=(len(self.model_array),1))
-        for ind1 in range(k):  
-            for ind2 in range(len(self.model_array)):
-                if self.reference_table_models[Indices[ind1]] == self.model_array[ind2]:
-                    count[ind2] = count[ind2] + 1
-
-        return(self.model_array[np.argmax(count)])
-
-    def posteriorprobability(self, observations, n_samples = 1000, n_samples_per_param = 100, k = 50):
-        """        
-        Parameters
-        ----------
-        data: python list
-            Contains n data sets.
-        n_samples : integer, optional
-            Number of samples to generate for reference table.
-        n_samples_per_param : integer, optional 
-            Number of data points in each simulated data set. 
-        k : integer, optional
-            The parameter for K-nearest neighbour classifier. The default calue if 50
-        Returns
-        -------
-        abcpy.models.Model
-            A model which are of type abcpy.models.Model
-            
-        """            
-        
-        # Creation of reference table
-        if self.reference_table_calculated is 0:        
-            rc = _RemoteContextgeneratereferencetable(self.backend, self.model_array, self.distance_calc, self.statistics_calc, observations, n_samples_per_param)
-
-            # Simulating the data, distance and statistics                
-            seed_arr = self.rng.randint(1, n_samples*n_samples, size=n_samples, dtype=np.int32)
-            seed_pds = self.backend.parallelize(seed_arr)     
-
-            model_data_pds = self.backend.map(rc._simulate_model_data, seed_pds)
-            model_data = self.backend.collect(model_data_pds)
-            models, data, distances, statistics = [list(t) for t in zip(*model_data)]
-            self.reference_table_models = models
-            self.reference_table_data = data
-            self.reference_table_distances = np.array(distances)
-            self.reference_table_statistics = statistics
-            self.reference_table_calculated = 1
-        
-        # Sort the array
-        Indices = np.array(sorted(range(len(self.reference_table_distances)), key=lambda k: self.reference_table_distances[k])).astype(int)
-        
-        # Compute the frequence of each model chosen in the k models having the smalles distance
-        count = np.zeros(shape=(len(self.model_array),1))
-        for ind1 in range(k):  
-            for ind2 in range(len(self.model_array)):
-                if self.reference_table_models[Indices[ind1]] == self.model_array[ind2]:
-                    count[ind2] = count[ind2] + 1
-
-        return(count/k)
         
 class RandomForest(ModelSelections):
     """
@@ -212,14 +95,13 @@ class RandomForest(ModelSelections):
     [1] Pudlo, P., Marin, J.-M., Estoup, A., Cornuet, J.-M., Gautier, M. and Robert, C.
     (2016). Reliable ABC model choice via random forests. Bioinformatics, 32 859–866.
     """
-    def __init__(self, model_array, statistics_calc, distance_calc, backend, seed = None):
+    def __init__(self, model_array, statistics_calc, backend, seed = None):
         self.model_array = model_array
         self.statistics_calc = statistics_calc
-        self.distance_calc = distance_calc
         self.backend = backend
         self.rng = np.random.RandomState(seed)
         self.seed = seed 
-        
+        self.reference_table_calculated = 0
 
     def modelchoice(self, observations, n_samples = 1000, n_samples_per_param = 1, N_tree = 100, n_try_fraction = 0.5):
         """        
@@ -228,14 +110,15 @@ class RandomForest(ModelSelections):
         data: python list
             Contains n data sets.
         n_samples : integer, optional
-            Number of samples to generate for reference table.
+            Number of samples to generate for reference table. The default value is 1000.
         n_samples_per_param : integer, optional 
-            Number of data points in each simulated data set. 
+            Number of data points in each simulated data set. The default value is 1.
         N_tree : integer, optional
-            Number of trees in the random forest. The default value if 100
+            Number of trees in the random forest. The default value is 100.
         n_try_fraction : float, optional 
             The fraction of number of summary statistics to be considered as the size of 
             the number of covariates randomly sampled at each node by the randomised CART.
+            he default value is 0.5.
         Returns
         -------
         abcpy.models.Model
@@ -244,7 +127,7 @@ class RandomForest(ModelSelections):
         """
         # Creation of reference table
         if self.reference_table_calculated is 0:        
-            rc = _RemoteContextgeneratereferencetable(self.backend, self.model_array, self.distance_calc, self.statistics_calc, observations, n_samples_per_param)
+            rc = _RemoteContextgeneratereferencetable(self.backend, self.model_array, self.statistics_calc, observations, n_samples_per_param)
 
             # Simulating the data, distance and statistics                
             seed_arr = self.rng.randint(1, n_samples*n_samples, size=n_samples, dtype=np.int32)
@@ -252,11 +135,10 @@ class RandomForest(ModelSelections):
 
             model_data_pds = self.backend.map(rc._simulate_model_data, seed_pds)
             model_data = self.backend.collect(model_data_pds)
-            models, data, distances, statistics = [list(t) for t in zip(*model_data)]
+            models, data, statistics = [list(t) for t in zip(*model_data)]
             self.reference_table_models = models
             self.reference_table_data = data
-            self.reference_table_distances = np.array(distances)
-            self.reference_table_statistics = statistics
+            self.reference_table_statistics = np.concatenate(statistics)
             self.reference_table_calculated = 1
 
         # Construct a label for the model_array
@@ -279,14 +161,15 @@ class RandomForest(ModelSelections):
         data: python list
             Contains n data sets.
         n_samples : integer, optional
-            Number of samples to generate for reference table.
+            Number of samples to generate for reference table. The default value is 1000.
         n_samples_per_param : integer, optional 
-            Number of data points in each simulated data set. 
+            Number of data points in each simulated data set. The default value is 1.
         N_tree : integer, optional
-            Number of trees in the random forest. The default value if 100
+            Number of trees in the random forest. The default value is 100.
         n_try_fraction : float, optional 
             The fraction of number of summary statistics to be considered as the size of 
             the number of covariates randomly sampled at each node by the randomised CART.
+            The default value is 0.5.
         Returns
         -------
         abcpy.models.Model
@@ -295,7 +178,7 @@ class RandomForest(ModelSelections):
         """        
         # Creation of reference table
         if self.reference_table_calculated is 0:        
-            rc = _RemoteContextgeneratereferencetable(self.backend, self.model_array, self.distance_calc, self.statistics_calc, observations, n_samples_per_param)
+            rc = _RemoteContextgeneratereferencetable(self.backend, self.model_array, self.statistics_calc, observations, n_samples_per_param)
 
             # Simulating the data, distance and statistics                
             seed_arr = self.rng.randint(1, n_samples*n_samples, size=n_samples, dtype=np.int32)
@@ -303,11 +186,10 @@ class RandomForest(ModelSelections):
 
             model_data_pds = self.backend.map(rc._simulate_model_data, seed_pds)
             model_data = self.backend.collect(model_data_pds)
-            models, data, distances, statistics = [list(t) for t in zip(*model_data)]
+            models, data, statistics = [list(t) for t in zip(*model_data)]
             self.reference_table_models = models
             self.reference_table_data = data
-            self.reference_table_distances = np.array(distances)
-            self.reference_table_statistics = statistics
+            self.reference_table_statistics = np.concatenate(statistics)
             self.reference_table_calculated = 1
         
         # Construct a label for the model_array
@@ -318,20 +200,27 @@ class RandomForest(ModelSelections):
                     label[ind1] = ind2 
 
         # Define the classifier 
-        classifier = RandomForestClassifier(n_estimators = N_tree, max_features=int(n_try_fraction*statistics.shape[1]), bootstrap=True, random_state=self.seed)
-        classifier.fit(self.reference_table_statistics, label)            
+        classifier = RandomForestClassifier(n_estimators = N_tree, max_features=int(n_try_fraction*self.reference_table_statistics.shape[1]), bootstrap=True, random_state=self.seed)
+        classifier.fit(self.reference_table_statistics, label)  
 
+        pred_error = np.zeros(len(self.reference_table_models),)
+        # Compute missclassification error rate
+        for ind in range(len(self.reference_table_models)):
+            pred_error[ind] = 1 - classifier.predict_proba(self.statistics_calc.statistics(self.reference_table_data[ind]))[0][int(label[ind])] 
 
-        return(classifier.predict_proba(self.statistics_calc.statistics(observations)))
+        # Estimate a regression function with prediction error as response on summary statitistics of the reference table                
+        regressor = RandomForestRegressor(n_estimators = N_tree)
+        regressor.fit(self.reference_table_statistics,pred_error)
+
+        return(1-regressor.predict(self.statistics_calc.statistics(observations)))
         
 class _RemoteContextgeneratereferencetable:
     """
     Contains everything that is sent over the network like broadcast vars and map functions
     """
     
-    def __init__(self, backend, model_array, dist_calc, stat_calc, observations, n_samples_per_param):       
+    def __init__(self, backend, model_array, stat_calc, observations, n_samples_per_param):       
         self.model_array = model_array
-        self.dist_calc = dist_calc
         self.stat_calc = stat_calc
         self.n_samples_per_param = n_samples_per_param
         
@@ -365,7 +254,6 @@ class _RemoteContextgeneratereferencetable:
         model.sample_from_prior()
         #sample data set, extract statistics and compute distance from y_obs
         y_sim = model.simulate(self.n_samples_per_param)
-        distance = self.dist_calc.distance(self.observations_bds.value(), y_sim)
         statistics = self.stat_calc.statistics(y_sim)
         
-        return (model, y_sim, distance, statistics)
+        return (model, y_sim, statistics)
--- tests/modelselections_tests.py
+++ tests/modelselections_tests.py
@@ -1,122 +1,44 @@
 import unittest
-import numpy as np
 from abcpy.distributions import Uniform
 from abcpy.models import Gaussian
 from abcpy.models import Student_t
 from abcpy.statistics import Identity
-from abcpy.distances import Euclidean
 from abcpy.backends import BackendDummy as Backend
-from abcpy.modelselections import Knn, RandomForest
-
-
-
-class KnnTests(unittest.TestCase):
-    def setUp(self):
-        # define observation for true parameters mean=170, std=15
-        self.y_obs = [160.82499176, 167.24266737, 185.71695756, 153.7045709, 163.40568812, 140.70658699, 169.59102084, 172.81041696, 187.38782738, 179.66358934, 176.63417241, 189.16082803, 181.98288443, 170.18565017, 183.78493886, 166.58387299, 161.9521899, 155.69213073, 156.17867343, 144.51580379, 170.29847515, 197.96767899, 153.36646527, 162.22710198, 158.70012047, 178.53470703, 170.77697743, 164.31392633, 165.88595994, 177.38083686, 146.67058471763457, 179.41946565658628, 238.02751620619537, 206.22458790620766, 220.89530574344568, 221.04082532837026, 142.25301427453394, 261.37656571434275, 171.63761180867033, 210.28121820385866, 237.29130237612236, 175.75558340169619, 224.54340549862235, 197.42448680731226, 165.88273684581381, 166.55094082844519, 229.54308602661584, 222.99844054358519, 185.30223966014586, 152.69149367593846, 206.94372818527413, 256.35498655339154, 165.43140916577741, 250.19273595481803, 148.87781549665536, 223.05547559193792, 230.03418198709608, 146.13611923127021, 138.24716809523139, 179.26755740864527, 141.21704876815426, 170.89587081800852, 222.96391329259626, 188.27229523693822, 202.67075179617672, 211.75963110985992, 217.45423324370509]
-        self.model_array = [None]*2
-        #Model 1: Gaussian
-        # define prior
-        prior = Uniform([150, 5],[200, 25])
-        # define the model
-        self.model_array[0] = Gaussian(prior)
-
-        #Model 2: Student t
-        # define prior
-        prior = Uniform([150, 1],[200, 30])
-        # define the model
-        self.model_array[1] = Student_t(prior)
-        # define statistics
-        self.statistics_calc = Identity(degree = 2, cross = False)
-        # define distance
-        self.distance_calc = Euclidean(self.statistics_calc)
-        # define backend
-        self.backend = Backend()
-
-
-    def test_modelchoice(self):
-        modelselection = Knn(model_array, statistics_calc, distance_calc, backend)
-        model = modelselection.modelchoice(y_obs)        
-        
-        self.assertRaises(TypeError, self.stat_calc.statistics, 3.4)
-        vec1 = np.array([1,2])
-        vec2 = np.array([1])
-        self.assertTrue((self.stat_calc.statistics([vec1]) == np.array([vec1])).all())
-        self.assertTrue((self.stat_calc.statistics([vec1,vec1]) == np.array([[vec1],[vec1]])).all())
-        self.assertTrue((self.stat_calc.statistics([vec2,vec2]) == np.array([[vec2],[vec2]])).all())
-    
-    def test_posteriorprobability(self):
-        model_prob = modelselection.posteriorprobability(y_obs)
-        #Checks whether wrong input type produces error message
-        self.assertRaises(TypeError, self.stat_calc._polynomial_expansion, 3.4)
-
-        a = [np.array([0, 2]),np.array([2,1])]             
-        # test cross-product part
-        self.stat_calc = Identity(degree = 2, cross = 1)
-        self.assertTrue((self.stat_calc.statistics(a) == np.array([[0,2,0,4,0],[2,1,4,1,2]])).all())
-        # When a tuple
-        a = [np.array([0, 2])] 
-        self.stat_calc = Identity(degree = 2, cross = 1)
-        self.assertTrue((self.stat_calc.statistics(a) == np.array([[0,2,0,4,0]])).all())
-        self.stat_calc = Identity(degree = 2, cross = 0)
-        self.assertTrue((self.stat_calc.statistics(a) == np.array([[0,2,0,4]])).all())
-        a = list(np.array([2])) 
-        self.stat_calc = Identity(degree = 2, cross = 1)
-        self.assertTrue((self.stat_calc.statistics(a) == np.array([[2,4]])).all())
+from abcpy.modelselections import RandomForest
      
 class RandomForestTests(unittest.TestCase):
     def setUp(self):
         # define observation for true parameters mean=170, std=15
-        self.y_obs = [160.82499176, 167.24266737, 185.71695756, 153.7045709, 163.40568812, 140.70658699, 169.59102084, 172.81041696, 187.38782738, 179.66358934, 176.63417241, 189.16082803, 181.98288443, 170.18565017, 183.78493886, 166.58387299, 161.9521899, 155.69213073, 156.17867343, 144.51580379, 170.29847515, 197.96767899, 153.36646527, 162.22710198, 158.70012047, 178.53470703, 170.77697743, 164.31392633, 165.88595994, 177.38083686, 146.67058471763457, 179.41946565658628, 238.02751620619537, 206.22458790620766, 220.89530574344568, 221.04082532837026, 142.25301427453394, 261.37656571434275, 171.63761180867033, 210.28121820385866, 237.29130237612236, 175.75558340169619, 224.54340549862235, 197.42448680731226, 165.88273684581381, 166.55094082844519, 229.54308602661584, 222.99844054358519, 185.30223966014586, 152.69149367593846, 206.94372818527413, 256.35498655339154, 165.43140916577741, 250.19273595481803, 148.87781549665536, 223.05547559193792, 230.03418198709608, 146.13611923127021, 138.24716809523139, 179.26755740864527, 141.21704876815426, 170.89587081800852, 222.96391329259626, 188.27229523693822, 202.67075179617672, 211.75963110985992, 217.45423324370509]
+        self.y_obs = [160.82499176]
         self.model_array = [None]*2
         #Model 1: Gaussian
         # define prior
         prior = Uniform([150, 5],[200, 25])
         # define the model
-        self.model_array[0] = Gaussian(prior)
-
+        self.model_array[0] = Gaussian(prior, seed = 1)
         #Model 2: Student t
         # define prior
         prior = Uniform([150, 1],[200, 30])
         # define the model
-        self.model_array[1] = Student_t(prior)
+        self.model_array[1] = Student_t(prior, seed = 1)
+
         # define statistics
         self.statistics_calc = Identity(degree = 2, cross = False)
-        # define distance
-        self.distance_calc = Euclidean(self.statistics_calc)
         # define backend
         self.backend = Backend()
 
 
     def test_modelchoice(self):
-        modelselection = RandomForest(model_array, statistics_calc, distance_calc, backend)
-        model = modelselection.modelchoice([y_obs[10]],n_samples = 100, n_samples_per_param = 1)
+        modelselection = RandomForest(self.model_array, self.statistics_calc, self.backend, seed = 1)
+        model = modelselection.modelchoice(self.y_obs,n_samples = 100, n_samples_per_param = 1)
 
-        
-        self.assertRaises(TypeError, self.stat_calc.statistics, 3.4)
-        vec1 = np.array([1,2])
-        vec2 = np.array([1])
-        self.assertTrue((self.stat_calc.statistics([vec1]) == np.array([vec1])).all())
-        self.assertTrue((self.stat_calc.statistics([vec1,vec1]) == np.array([[vec1],[vec1]])).all())
-        self.assertTrue((self.stat_calc.statistics([vec2,vec2]) == np.array([[vec2],[vec2]])).all())
+        self.assertTrue(self.model_array[1] == model)
     
     def test_posteriorprobability(self):
-        modelselection = RandomForest(model_array, statistics_calc, distance_calc, backend)
-        model_prob = modelselection.posteriorprobability([y_obs[10]])
+        modelselection = RandomForest(self.model_array, self.statistics_calc, self.backend, seed = 1)
+        model_prob = modelselection.posteriorprobability(self.y_obs)
 
-        self.assertRaises(TypeError, self.stat_calc._polynomial_expansion, 3.4)
-        a = [np.array([0, 2]),np.array([2,1])]             
-        # test cross-product part
-        self.stat_calc = Identity(degree = 2, cross = 1)
-        self.assertTrue((self.stat_calc.statistics(a) == np.array([[0,2,0,4,0],[2,1,4,1,2]])).all())
-        # When a tuple
-        a = [np.array([0, 2])] 
-        self.stat_calc = Identity(degree = 2, cross = 1)
-        self.assertTrue((self.stat_calc.statistics(a) == np.array([[0,2,0,4,0]])).all())
-        self.stat_calc = Identity(degree = 2, cross = 0)
-        self.assertTrue((self.stat_calc.statistics(a) == np.array([[0,2,0,4]])).all())
-        a = list(np.array([2])) 
-        self.stat_calc = Identity(degree = 2, cross = 1)
-        self.assertTrue((self.stat_calc.statistics(a) == np.array([[2,4]])).all())     
+        self.assertTrue(model_prob == 0.8238)
+ 
 if __name__ == '__main__':
     unittest.main()
