8bd986ba984e4795f31be49aadd9258f6f2ad818
Nicole <widmern@student.ethz.ch>
2017-11-14 17:48:57 +0100

    Finished implementations, rejectionabc and pmcabc working

--- abcpy/test_graph_methods.py
+++ abcpy/test_graph_methods.py
@@ -9,6 +9,7 @@ from perturbationkernel import *
 
 """Tests whether the methods defined for operations on the graph work as intended."""
 
+
 class SampleFromPriorTests(unittest.TestCase):
     """Tests whether sample_from_prior assigns new values to all nodes corresponding to free parameters in the graph."""
     def test(self):
@@ -30,6 +31,7 @@ class SampleFromPriorTests(unittest.TestCase):
         self.assertIsNotNone(N1.fixed_values)
         self.assertIsNotNone(N2.fixed_values)
 
+
 class ResetFlagsTests(unittest.TestCase):
     """Tests whether it is possible to reset all visited flags in the graph."""
     def test(self):
@@ -48,6 +50,7 @@ class ResetFlagsTests(unittest.TestCase):
 
         self.assertFalse(N1.visited)
         self.assertFalse(N2.visited)
+
 
 class GetParametersTests(unittest.TestCase):
     """Tests whether get_parameters returns only the free parameters of the graph."""
@@ -70,6 +73,7 @@ class GetParametersTests(unittest.TestCase):
     def test(self):
         free_parameters = self.sampler.get_parameters()
         self.assertEqual(len(free_parameters),3)
+
 
 class SetParametersTests(unittest.TestCase):
     """Tests whether it is possible to set values for all free parameters of the graph."""
@@ -121,6 +125,7 @@ class GetCorrectOrderingTests(unittest.TestCase):
         ordered_parameters = self.sampler.get_correct_ordering(parameters_and_models)
         self.assertEqual(ordered_parameters, [3,0.12,0.029])
 
+
 class PerturbTests(unittest.TestCase):
     """Tests whether perturb will change all fixed values for free parameters."""
     def setUp(self):
@@ -142,7 +147,7 @@ class PerturbTests(unittest.TestCase):
         kernel = StandardKernel([self.N1, self.N2, self.B1])
         self.sampler.kernel = kernel
 
-        self.sampler.accepted_parameters_manager.update_broadcast(self.sampler.backend, [[3,4],[0.11,0.098],[0.029,0.031]], [1,1])
+        self.sampler.accepted_parameters_manager.update_broadcast(self.sampler.backend, [[3, 0.11, 0.029],[4,0.098, 0.031]], np.array([1,1]))
 
     def test(self):
         B1_value = self.B1.fixed_values
@@ -155,31 +160,93 @@ class PerturbTests(unittest.TestCase):
         self.assertNotEqual(N1_value, self.N1.fixed_values)
         self.assertNotEqual(N2_value, self.N2.fixed_values)
 
-class GetProbabilisticModelsCorrespondingToFreeParametersTests(unittest.TestCase):
-    """Tests whether get_probabilistic_models_corresponding_to_free_parameters returns all probabilistic models corresponding to free parameters of a given type."""
-    def setUp(self):
-        self.B1 = Binomial([10, 0.2])
-        self.N1 = Normal([0.03, 0.01])
-        self.N2 = Normal([0.1, self.N1])
-        self.graph = Normal([self.B1, self.N2])
+
+class SimulateTests(unittest.TestCase):
+    """Tests whether the simulated data for multiple models has the correct format."""
+    def test(self):
+        B1 = Binomial([10, 0.2])
+        N1 = Normal([0.03, 0.01])
+        N2 = Normal([0.1, N1])
+        graph1 = Normal([B1, N2])
+        graph2 = Normal([1,N2])
 
         statistics_calculator = Identity(degree=2, cross=False)
         distance_calculator = LogReg(statistics_calculator)
         backend = Backend()
 
-        self.sampler = RejectionABC([self.graph], distance_calculator, backend)
+        sampler = RejectionABC([graph1, graph2], distance_calculator, backend)
 
-        self.rng = np.random.RandomState(1)
+        rng = np.random.RandomState(1)
 
-    def test_continuous(self):
-        continuous_models = self.sampler.get_probabilistic_models_corresponding_to_free_parameters(Continuous)
-        self.assertEqual(continuous_models, [self.N2,self.N1])
+        sampler.sample_from_prior(rng=rng)
 
-    def test_discrete(self):
-        discrete_models = self.sampler.get_probabilistic_models_corresponding_to_free_parameters(Discrete)
-        self.assertEqual(discrete_models, [self.B1])
+        y_sim = sampler.simulate(rng=rng)
+
+        self.assertTrue(isinstance(y_sim, list))
+
+        self.assertTrue(len(y_sim)==2)
+
+        self.assertTrue(isinstance(y_sim[0], np.ndarray))
 
 
+class GetMappingTests(unittest.TestCase):
+    """Tests whether the private get_mapping method will return the correct mapping."""
+    def test(self):
+        B1 = Binomial([10, 0.2])
+        N1 = Normal([0.03, 0.01])
+        N2 = Normal([0.1, N1])
+        graph1 = Normal([B1, N2])
+        graph2 = Normal([1, N2])
+
+        statistics_calculator = Identity(degree=2, cross=False)
+        distance_calculator = LogReg(statistics_calculator)
+        backend = Backend()
+
+        sampler = RejectionABC([graph1, graph2], distance_calculator, backend)
+
+        rng = np.random.RandomState(1)
+
+        sampler.sample_from_prior(rng=rng)
+
+        mapping, index = sampler._get_mapping()
+        self.assertTrue(mapping==[(B1, 0),(N2, 1),(N1,2)])
+
+
+class PdfOfPriorTests(unittest.TestCase):
+    """Tests the implemetation of pdf_of_prior"""
+    def setUp(self):
+        class Mockobejct(Normal):
+            def __init__(self, parameters):
+                super(Mockobejct, self).__init__(parameters)
+            def pdf(self, x):
+                return x
+        self.N1 = Mockobject([1,0.1])
+        self.N2 = Mockobject([self.N1, 0.1])
+        self.N3 = Mockobject([0.1,0.01])
+        self.graph1 = Mockobject([self.N2, self.N3])
+        self.graph2 = Mockobject([2,self.N3])
+
+        self.graph = [self.graph1, self.graph2]
+
+        statistics_calculator = Identity(degree=2, cross=False)
+        distance_calculator = LogReg(statistics_calculator)
+        backend = Backend()
+
+        self.sampler = RejectionABC(self.graph, distance_calculator, backend)
+
+        rng = np.random.RandomState(1)
+
+        self.sampler.sample_from_prior(rng=rng)
+
+        self.pdf = self.sampler.pdf_of_prior(self.sampler.model, [1, 2, 4])
+
+    def test_return_value(self):
+        """Tests whether the return value is float."""
+        self.assertTrue(isinstance(self.pdf, float))
+
+    def test_result(self):
+        """Test whether pdf calculation works as intended"""
+        self.assertTrue(self.pdf==32)
 
 
 if __name__ == '__main__':
--- abcpy/test_perturbationkernel.py
+++ abcpy/test_perturbationkernel.py
@@ -8,6 +8,25 @@ from perturbationkernel import *
 """Tests whether the methods for each perturbation kernel are working as intended"""
 
 
+class JointCheckKernelsTests(unittest.TestCase):
+    """Tests whether value errors are raised correctly during initialization."""
+    def test_Raises(self):
+        N1 = Normal([0.1, 0.01])
+        N2 = Normal([0.3, N1])
+        kernel = MultivariateNormalKernel([N1,N2,N1])
+        with self.assertRaises(ValueError):
+            JointPerturbationKernel([kernel])
+
+    def test_doesnt_raise(self):
+        N1 = Normal([0.1, 0.01])
+        N2 = Normal([0.3, N1])
+        kernel = MultivariateNormalKernel([N1, N2])
+        try:
+            JointPerturbationKernel([kernel])
+        except ValueError:
+            self.fail("JointPerturbationKernel raises an exception")
+
+
 class UpdateTests(unittest.TestCase):
     """Tests whether the values returned after perturbation are in the correct format for each perturbation kernel."""
     def test_StandardKernel(self):
@@ -19,13 +38,28 @@ class UpdateTests(unittest.TestCase):
         Manager = AcceptedParametersManager([graph])
         backend = Backend()
         kernel = StandardKernel([N1, N2, B1])
-        Manager.update_broadcast(backend, [[2, 3], [0.27, 0.32], [0.97, 0.12]], [1,1])
+        Manager.update_broadcast(backend, [[2, 0.27, 0.097], [3, 0.32, 0.012]], np.array([1,1]))
 
         rng = np.random.RandomState(1)
         perturbed_values_and_models = kernel.update(Manager, 1, rng)
+        self.assertEqual(perturbed_values_and_models, [(N1, [-0.085629777838992588]), (N2, [0.37742928108176033]), (B1, [3])])
 
-        self.assertEqual(perturbed_values_and_models, [(N1, [-0.85629777838992582]), (N2, [0.37742928108176033]), (B1, [3])])
 
+class PdfTests(unittest.TestCase):
+    """Tests whether the pdf returns the correct results."""
+    def test(self):
+        B1 = Binomial([10, 0.2])
+        N1 = Normal([0.1, 0.01])
+        N2 = Normal([0.3, N1])
+        graph = Normal([B1, N2])
+
+        Manager = AcceptedParametersManager([graph])
+        backend = Backend()
+        kernel = StandardKernel([N1, N2, B1])
+        Manager.update_broadcast(backend, [[2, 0.27, 0.097], [3, 0.32, 0.012]], np.array([1, 1]))
+
+        pdf = kernel.pdf(Manager, 1, [2,0.3,0.1])
+        self.assertTrue(isinstance(pdf, float))
 
 if __name__ == '__main__':
     unittest.main()
