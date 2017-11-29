c22884bf2b434f55d9c119ef73150d1247f5381b
statrita <statrita2004@gmail.com>
2017-11-29 11:19:08 +0100

    Summary selections

--- abcpy/summaryselections.py
+++ abcpy/summaryselections.py
@@ -1,13 +1,18 @@
 from abc import ABCMeta, abstractmethod
+
+from abcpy.graphtools import GraphTools
+from abcpy.acceptedparametersmanager import *
+
 import numpy as np
 from sklearn import linear_model
 
-class Summaryselections(metaclass = ABCMeta):
+
+class Summaryselections(metaclass=ABCMeta):
     """This abstract base class defines a way to choose the summary statistics.
     """
-    
+
     @abstractmethod
-    def __init__(self, model, statistics_calc, backend, n_samples = 1000, seed = None):
+    def __init__(self, model, statistics_calc, backend, n_samples=1000, seed=None):
         """The constructor of a sub-class must accept a non-optional model, statistics calculator and 
         backend which are stored to self.model, self.statistics_calc and self.backend. Further it 
         accepts two optional parameters n_samples and seed defining the number of simulated dataset
@@ -34,49 +39,65 @@ class Summaryselections(metaclass = ABCMeta):
         state = self.__dict__.copy()
         del state['backend']
         return state
-        
-        
+
     @abstractmethod
     def transformation(self, statistics):
         raise NotImplementedError
 
 
-class Semiautomatic(Summaryselections):
+class Semiautomatic(Summaryselections, GraphTools):
     """This class implements the semi automatic summary statistics choice described in Fearnhead and Prangle [1].
     
     [1] Fearnhead P., Prangle D. 2012. Constructing summary statistics for approximate
     Bayesian computation: semi-automatic approximate Bayesian computation. J. Roy. Stat. Soc. B 74:419–474.    
     """
 
-    def __init__(self, model, statistics_calc, backend, n_samples = 1000, seed = None):
+    def __init__(self, model, statistics_calc, backend, n_samples=1000, seed=None):
         self.model = model
         self.statistics_calc = statistics_calc
         self.backend = backend
         self.rng = np.random.RandomState(seed)
-        self.model.prior.reseed(self.rng.randint(np.iinfo(np.uint32).max, dtype=np.uint32)) 
+        self.n_samples_per_param = 1
 
-        # main algorithm                 
-        seed_arr = self.rng.randint(1, n_samples*n_samples, size=n_samples, dtype=np.int32)
-        seed_pds = self.backend.parallelize(seed_arr)     
+        #Test part, after y_sim is not None, we remove
+        rng = np.random.RandomState(seed)
+        self.sample_from_prior(rng=rng)
+        print('checkedparam')
+        print(self.get_parameters())
+        print('checkeddata')
+        y_sim = None
+        while y_sim is None:
+            y_sim = self.simulate(rng=rng)
+            print(y_sim)
 
-        sample_parameters_statistics_pds = self.backend.map(self._sample_parameter_statistics, seed_pds)
+            # An object managing the bds objects
+        self.accepted_parameters_manager = AcceptedParametersManager(self.model)
+        self.accepted_parameters_manager.broadcast(self.backend, [])
+
+        # main algorithm
+        seed_arr = self.rng.randint(1, n_samples * n_samples, size=n_samples, dtype=np.int32)
+        rng_arr = np.array([np.random.RandomState(seed) for seed in seed_arr])
+        rng_pds = self.backend.parallelize(rng_arr)
+
+        sample_parameters_statistics_pds = self.backend.map(self._sample_parameter_statistics, rng_pds)
+
         sample_parameters_and_statistics = self.backend.collect(sample_parameters_statistics_pds)
         sample_parameters, sample_statistics = [list(t) for t in zip(*sample_parameters_and_statistics)]
         sample_parameters = np.array(sample_parameters)
         sample_statistics = np.concatenate(sample_statistics)
-        
-        self.coefficients_learnt = np.zeros(shape=(sample_parameters.shape[1],sample_statistics.shape[1]))
+
+        self.coefficients_learnt = np.zeros(shape=(sample_parameters.shape[1], sample_statistics.shape[1]))
         regr = linear_model.LinearRegression(fit_intercept=True)
         for ind in range(sample_parameters.shape[1]):
-            regr.fit(sample_statistics, sample_parameters[:,ind]) 
-            self.coefficients_learnt[ind,:] = regr.coef_ 
-        
-    def transformation(self, statistics):
-        if not statistics.shape[1] == self.coefficients_learnt.shape[1]:    
-            raise ValueError('Mismatch in dimension of summary statistics')
-        return np.dot(statistics,np.transpose(self.coefficients_learnt))
+            regr.fit(sample_statistics, sample_parameters[:, ind])
+            self.coefficients_learnt[ind, :] = regr.coef_
 
-    def _sample_parameter_statistics(self, seed):
+    def transformation(self, statistics):
+        if not statistics.shape[1] == self.coefficients_learnt.shape[1]:
+            raise ValueError('Mismatch in dimension of summary statistics')
+        return np.dot(statistics, np.transpose(self.coefficients_learnt))
+
+    def _sample_parameter_statistics(self, rng=np.random.RandomState()):
         """
         Samples a single model parameter and simulates from it until
         distance between simulated outcome and the observation is
@@ -91,13 +112,13 @@ class Semiautomatic(Summaryselections):
         np.array
             accepted parameter
         """
-        self.model.prior.reseed(seed)
-        self.model.sample_from_prior()
 
-        return (self.model.get_parameters(), self.statistics_calc.statistics(self.model.simulate(1)))
-
-        
-
-        
-        
-        
+        self.sample_from_prior(rng=rng)
+        print('checked')
+        print(self.get_parameters())
+        parameter = self.get_parameters()
+        print('checked')
+        y_sim = self.simulate(rng=rng)
+        if y_sim is not None:
+            statistics = self.statistics_calc.statistics(y_sim)
+        return (parameter, statistics)
