1ad9ced0349910a7a2ad61ebdb50640e29e690a7
duttar <duttar@duttar.stas.warwick.ac.uk>
2019-01-08 11:54:11 +0000

    Removed untested distances

--- tests/distances_tests.py
+++ tests/distances_tests.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from abcpy.distances import Euclidean, PenLogReg, LogReg, KLdistance, Abserror
+from abcpy.distances import Euclidean, PenLogReg, LogReg
 from abcpy.statistics import Identity
 
 class EuclideanTests(unittest.TestCase):
