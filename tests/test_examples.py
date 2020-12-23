8bd200c5fba2b991b3150334e26af2b5ab0fffaf
LoryPack <lorenzo.pacchiardi@stats.ox.ac.uk>
2020-12-23 11:18:19 +0100

    Fix tests without Pytorch

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,6 +1,12 @@
 """Tests here the examples which do not require parallelization."""
 
 import unittest
+try:
+    import torch
+except ImportError:
+    has_torch = False
+else:
+    has_torch = True
 
 
 class ExampleApproxLhdTest(unittest.TestCase):
@@ -83,11 +89,12 @@ class ExampleModelSelectionTest(unittest.TestCase):
 
 class ExampleStatisticsLearningTest(unittest.TestCase):
     def test_pmcabc(self):
-        from examples.statisticslearning.pmcabc_gaussian_statistics_learning import infer_parameters
-        journal = infer_parameters(steps=1, n_sample=50)
-        test_result = journal.posterior_mean()["mu"]
-        expected_result = 172.52136853079725
-        self.assertAlmostEqual(test_result, expected_result)
+        if has_torch:
+            from examples.statisticslearning.pmcabc_gaussian_statistics_learning import infer_parameters
+            journal = infer_parameters(steps=1, n_sample=50)
+            test_result = journal.posterior_mean()["mu"]
+            expected_result = 172.52136853079725
+            self.assertAlmostEqual(test_result, expected_result)
 
 
 if __name__ == '__main__':
