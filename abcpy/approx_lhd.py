ee08b9f3ed47df82fb44a80f3c1074cb12975625
Lorenzo Pacchiardi <lorenzo.pacchiardi@stats.ox.ac.uk>
2021-03-11 11:24:39 +0100

    Docstring

--- abcpy/approx_lhd.py
+++ abcpy/approx_lhd.py
@@ -56,9 +56,41 @@ class Approx_likelihood(metaclass=ABCMeta):
         raise NotImplemented
 
     def likelihood(self, y_obs, y_sim):
+        """Computes the likelihood by taking the exponential of the loglikelihood method.
+
+        Parameters
+        ----------
+        y_obs: Python list
+            Observed data set.
+        y_sim: Python list
+            Simulated data set from model at the parameter value.
+
+        Returns
+        -------
+        float
+            Computed approximate likelihood.
+
+        """
         return np.exp(self.loglikelihood(y_obs, y_sim))
 
     def _calculate_summary_stat(self, y_obs, y_sim):
+        """Helper function that extracts the summary statistics s_obs and s_sim from y_obs and
+        y_y_sim using the statistics object stored in self.statistics_calc. This stores s_obs for the purpose of checking
+        whether that is repeated in next calls to the function, and avoiding computing the statitistics for the same
+        dataset several times.
+
+        Parameters
+        ----------
+        y_obs : array-like
+            d1 contains n_obs data sets.
+        y_sim : array-like
+            d2 contains n_sim data sets.
+
+        Returns
+        -------
+        tuple
+            Tuple containing numpy.ndarray's with the summary statistics extracted from d1 and d2.
+        """
         if not isinstance(y_obs, list):
             raise TypeError('Observed data is not of allowed types')
 
@@ -91,22 +123,45 @@ class Approx_likelihood(metaclass=ABCMeta):
 
 
 class SynLikelihood(Approx_likelihood):
-    """This class implements the approximate likelihood function which computes the approximate
-    likelihood using the synthetic likelihood approach described in Wood [1].
-    For synthetic likelihood approximation, we compute the robust precision matrix using Ledoit and Wolf's [2]
-    method.
-    
-    [1] S. N. Wood. Statistical inference for noisy nonlinear ecological 
-    dynamic systems. Nature, 466(7310):1102–1104, Aug. 2010.
-    
-    [2] O. Ledoit and M. Wolf, A Well-Conditioned Estimator for Large-Dimensional Covariance Matrices,
-    Journal of Multivariate Analysis, Volume 88, Issue 2, pages 365-411, February 2004.
-    """
 
     def __init__(self, statistics_calc):
+        """This class implements the approximate likelihood function which computes the approximate
+        likelihood using the synthetic likelihood approach described in Wood [1].
+        For synthetic likelihood approximation, we compute the robust precision matrix using Ledoit and Wolf's [2]
+        method.
+
+        [1] S. N. Wood. Statistical inference for noisy nonlinear ecological
+        dynamic systems. Nature, 466(7310):1102–1104, Aug. 2010.
+
+        [2] O. Ledoit and M. Wolf, A Well-Conditioned Estimator for Large-Dimensional Covariance Matrices,
+        Journal of Multivariate Analysis, Volume 88, Issue 2, pages 365-411, February 2004.
+
+
+        Parameters
+        ----------
+        statistics_calc : abcpy.statistics.Statistics
+            Statistics extractor object that conforms to the Statistics class.
+        """
+
         super(SynLikelihood, self).__init__(statistics_calc)
 
     def loglikelihood(self, y_obs, y_sim):
+        """Computes the loglikelihood.
+
+        Parameters
+        ----------
+        y_obs: Python list
+            Observed data set.
+        y_sim: Python list
+            Simulated data set from model at the parameter value.
+
+        Returns
+        -------
+        float
+            Computed approximate loglikelihood.
+
+        """
+
         stat_obs, stat_sim = self._calculate_summary_stat(y_obs, y_sim)
 
         # Compute the mean, robust precision matrix and determinant of precision matrix
@@ -129,17 +184,32 @@ class SynLikelihood(Approx_likelihood):
 
 
 class SemiParametricSynLikelihood(Approx_likelihood):
-    """This class implements the approximate likelihood function which computes the approximate
-    likelihood using the semiparametric Synthetic Likelihood (semiBSL) approach described in [1].
-
-    This does not yet include shrinkage strategies for the correlation matrix.
-
-    [1] An, Z., Nott, D. J., & Drovandi, C. (2020). Robust Bayesian synthetic likelihood via a semi-parametric approach.
-    Statistics and Computing, 30(3), 543-557.
-
-    """
 
     def __init__(self, statistics_calc, bw_method_marginals="silverman"):
+        """
+        This class implements the approximate likelihood function which computes the approximate
+        likelihood using the semiparametric Synthetic Likelihood (semiBSL) approach described in [1]. Specifically, this
+        represents the likelihood as a product of univariate marginals and the copula components (exploiting Sklar's
+        theorem).
+        The marginals are approximated from simulations using a Gaussian KDE, while the copula is assumed to be a Gaussian
+        copula, whose parameters are estimated from data as well.
+
+        This does not yet include shrinkage strategies for the correlation matrix.
+
+        [1] An, Z., Nott, D. J., & Drovandi, C. (2020). Robust Bayesian synthetic likelihood via a semi-parametric approach.
+        Statistics and Computing, 30(3), 543-557.
+
+        Parameters
+        ----------
+        statistics_calc : abcpy.statistics.Statistics
+            Statistics extractor object that conforms to the Statistics class.
+        bw_method_marginals : str, scalar or callable, optional
+            The method used to calculate the estimator bandwidth, passed to `scipy.stats.gaussian_kde`. Following the docs
+            of that method, this can be 'scott', 'silverman', a scalar constant or a callable. If a scalar, this will be
+            used directly as `kde.factor`. If a callable, it should take a `gaussian_kde` instance as only parameter
+            and return a scalar. If None (default), 'silverman' is used. See the Notes in `scipy.stats.gaussian_kde`
+            for more details.
+        """
         super(SemiParametricSynLikelihood, self).__init__(statistics_calc)
         # create a dict in which store the denominator of the correlation matrix for the different n values;
         # this saves from repeating computations:
@@ -147,7 +217,21 @@ class SemiParametricSynLikelihood(Approx_likelihood):
         self.bw_method_marginals = bw_method_marginals  # the bw method to use in the gaussian_kde
 
     def loglikelihood(self, y_obs, y_sim):
-        # this implementation aims to be equivalent to the 'BSL' R package
+        """Computes the loglikelihood. This implementation aims to be equivalent to the `BSL` R package,
+        but the results are slightly different due to small differences in the way the KDE is performed
+
+        Parameters
+        ----------
+        y_obs: Python list
+            Observed data set.
+        y_sim: Python list
+            Simulated data set from model at the parameter value.
+
+        Returns
+        -------
+        float
+            Computed approximate loglikelihood.
+        """
 
         stat_obs, stat_sim = self._calculate_summary_stat(y_obs, y_sim)
         n_obs, d = stat_obs.shape
@@ -190,7 +274,19 @@ class SemiParametricSynLikelihood(Approx_likelihood):
         return loglik
 
     def _estimate_gaussian_correlation(self, x):
-        # this is checked with the BSL library, same result
+        """Estimates the correlation matrix using data in `x` in the way described in [1]. This implementation
+        gives the same results as the `BSL` R package.
+
+        Parameters
+        ----------
+        x: np.ndarray
+            Data set.
+
+        Returns
+        -------
+        np.ndarray
+            Estimated correlation matrix for the gaussian copula.
+        """
         n, d = x.shape
         r = np.zeros_like(x)
         for j in range(d):
@@ -209,43 +305,43 @@ class SemiParametricSynLikelihood(Approx_likelihood):
 
 
 class PenLogReg(Approx_likelihood, GraphTools):
-    """This class implements the approximate likelihood function which computes the approximate
-    likelihood up to a constant using penalized logistic regression described in
-    Dutta et. al. [1]. It takes one additional function handler defining the 
-    true model and two additional parameters n_folds and n_simulate correspondingly defining number
-    of folds used to estimate prediction error using cross-validation and the number 
-    of simulated dataset sampled from each parameter to approximate the likelihood
-    function. For lasso penalized logistic regression we use glmnet of Friedman et.
-    al. [2].
-    
-    [1] Thomas, O., Dutta, R., Corander, J., Kaski, S., & Gutmann, M. U. (2020).
-    Likelihood-free inference by ratio estimation. Bayesian Analysis.
-    
-    [2] Friedman, J., Hastie, T., and Tibshirani, R. (2010). Regularization 
-    paths for generalized linear models via coordinate descent. Journal of Statistical 
-    Software, 33(1), 1–22.
-
-    Parameters
-    ----------
-    statistics_calc : abcpy.statistics.Statistics
-        Statistics extractor object that conforms to the Statistics class.
-    model : abcpy.models.Model
-        Model object that conforms to the Model class.
-    n_simulate : int
-        Number of data points to simulate for the reference data set; this has to be the same as n_samples_per_param
-        when calling the sampler. The reference data set is generated by drawing parameters from the prior and samples
-        from the model when PenLogReg is instantiated.
-    n_folds: int, optional
-        Number of folds for cross-validation. The default value is 10.
-    max_iter: int, optional
-        Maximum passes over the data. The default is 100000.
-    seed: int, optional
-        Seed for the random number generator. The used glmnet solver is not
-        deterministic, this seed is used for determining the cv folds. The default value is
-        None.
-    """
 
     def __init__(self, statistics_calc, model, n_simulate, n_folds=10, max_iter=100000, seed=None):
+        """This class implements the approximate likelihood function which computes the approximate
+        likelihood up to a constant using penalized logistic regression described in
+        Dutta et. al. [1]. It takes one additional function handler defining the
+        true model and two additional parameters n_folds and n_simulate correspondingly defining number
+        of folds used to estimate prediction error using cross-validation and the number
+        of simulated dataset sampled from each parameter to approximate the likelihood
+        function. For lasso penalized logistic regression we use glmnet of Friedman et.
+        al. [2].
+
+        [1] Thomas, O., Dutta, R., Corander, J., Kaski, S., & Gutmann, M. U. (2020).
+        Likelihood-free inference by ratio estimation. Bayesian Analysis.
+
+        [2] Friedman, J., Hastie, T., and Tibshirani, R. (2010). Regularization
+        paths for generalized linear models via coordinate descent. Journal of Statistical
+        Software, 33(1), 1–22.
+
+        Parameters
+        ----------
+        statistics_calc : abcpy.statistics.Statistics
+            Statistics extractor object that conforms to the Statistics class.
+        model : abcpy.models.Model
+            Model object that conforms to the Model class.
+        n_simulate : int
+            Number of data points to simulate for the reference data set; this has to be the same as n_samples_per_param
+            when calling the sampler. The reference data set is generated by drawing parameters from the prior and
+            samples from the model when PenLogReg is instantiated.
+        n_folds: int, optional
+            Number of folds for cross-validation. The default value is 10.
+        max_iter: int, optional
+            Maximum passes over the data. The default is 100000.
+        seed: int, optional
+            Seed for the random number generator. The used glmnet solver is not
+            deterministic, this seed is used for determining the cv folds. The default value is
+            None.
+        """
 
         super(PenLogReg, self).__init__(statistics_calc)  # call the super init to initialize correctly
 
@@ -259,6 +355,20 @@ class PenLogReg(Approx_likelihood, GraphTools):
         self.ref_data_stat = self._simulate_ref_data(rng=self.rng)[0]
 
     def loglikelihood(self, y_obs, y_sim):
+        """Computes the loglikelihood.
+
+        Parameters
+        ----------
+        y_obs: Python list
+            Observed data set.
+        y_sim: Python list
+            Simulated data set from model at the parameter value.
+
+        Returns
+        -------
+        float
+            Computed approximate loglikelihood.
+        """
         stat_obs, stat_sim = self._calculate_summary_stat(y_obs, y_sim)
 
         if not stat_sim.shape[0] == self.n_simulate:
@@ -283,6 +393,17 @@ class PenLogReg(Approx_likelihood, GraphTools):
         """
         Simulate the reference data set. This code is run at the initialization of
         Penlogreg
+
+        Parameters
+        ----------
+        rng: Random number generator, optional
+            Defines the random number generator to be used. If None, a newly initialized one is used
+
+        Returns
+        -------
+        list
+            The simulated list of datasets.
+
         """
 
         ref_data_stat = [[None] * self.n_simulate for i in range(len(self.model))]
