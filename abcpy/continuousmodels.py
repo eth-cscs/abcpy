Fix the gradient output of Normal to be same shape as that of generative nets

abcpy/continuousmodels.py
@@ -203,7 +203,7 @@ class Normal(ProbabilisticModel, Continuous):
         grad = np.ones((k, 2))
         grad[:, 1] = z
 
-        return [np.array([x]).reshape(-1, ) for x in result], [np.array([x]).reshape(-1, ) for x in grad]
+        return [np.array([x]).reshape(-1, ) for x in result], [np.array([x]).reshape(1, 2) for x in grad]
 
     def get_output_dimension(self):
         return 1

tests/continuousmodels_tests.py
@@ -195,8 +195,11 @@ class GradientsTest(unittest.TestCase):
         samples2, gradients = N.forward_simulate_and_gradient(N.get_input_values(), 3, rng=np.random.RandomState(0))
         self.assertTrue(np.all(samples == samples))
         self.assertTrue(len(samples2) == len(gradients))
-        self.assertTrue(gradients[0].shape[0] == 2)
-        self.assertTrue(gradients[0][0] == 1)  # the gradient wrt my is always 1 for the normal model
+        # check if gradients[0] has 2 dimensions:
+        self.assertTrue(2 == len(gradients[0].shape))
+        self.assertTrue(gradients[0].shape[0] == 1)  # 1d output for each simulation
+        self.assertTrue(gradients[0].shape[1] == 2)  # 2d parameters
+        self.assertTrue(gradients[0][0, 0] == 1)  # the gradient wrt my is always 1 for the normal model
 
 
 class CheckParametersBeforeSamplingTests(unittest.TestCase):